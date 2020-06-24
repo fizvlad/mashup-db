5fd509c06a449e472362b5220ffe38761a665ec1
Fizvlad <fizvlad@mail.ru>
2020-06-24 14:32:19 +0300

    Prefer using Mashup#track method instead of Mashup#audio

--- app/models/mashup.rb
+++ app/models/mashup.rb
@@ -1,6 +1,11 @@
 class Mashup < ApplicationRecord
-  belongs_to :audio
-
   # NOTE: it is possible to use inheritance of Mashup from Audio, but I would
   #   like to do it in simplier way and just explicitly store ID of track
+
+  # NOTE: be careful using #audio and #audios. First method returnes mashup
+  #   itself, while second method returnes list of its sources. Prefer using
+  #   #track and #sources instead.
+
+  belongs_to :audio
+  alias_attribute :track, :audio
 end
--- test/models/mashup_test.rb
+++ test/models/mashup_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class MashupTest < ActiveSupport::TestCase
   test 'audio exist' do
-    assert mashups(:new_year_miracle).audio
-    assert mashups(:new_year_miracle).audio.artist
+    assert mashups(:new_year_miracle).track
+    assert mashups(:new_year_miracle).track.artist
   end
 end
