9bc2652d60800b0668d1def4fd4beebb7cce65c4
Fizvlad <fizvlad@mail.ru>
2020-06-26 01:32:56 +0300

    Add primitive user views

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+
   root to: 'pages#index'
+
+  resources :users, except: %i[new index]
+  get 'signup', to: 'users#new'
 end
