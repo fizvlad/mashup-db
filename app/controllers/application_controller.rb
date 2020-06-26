3a2820dfda85d891d30d83c95088f1031ed56d85
Fizvlad <fizvlad@mail.ru>
2020-06-26 12:50:06 +0300

    Add flash alerts helper

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,14 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  helper_method :push_to_flash_alerts
+
+  private
+
+  # Allowed types: 'primary', 'secondary', 'success', 'danger', 'warning', 'info', 'light', 'dark'
+  def push_to_flash_alerts(text = '', type: 'info')
+    flash[:alerts] ||= []
+    flash[:alerts] << { 'type' => type.to_s, 'text' => text.to_s }
+  end
 end
