2c55ace04905f3a52b4d49aee7bc8ed6661456f7
Fizvlad <fizvlad@mail.ru>
2020-06-26 13:03:19 +0300

    Require login for actions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,11 +5,18 @@ class ApplicationController < ActionController::Base
 
   helper_method :push_to_flash_alerts
 
+  before_action :require_login
+
   private
 
   # Allowed types: 'primary', 'secondary', 'success', 'danger', 'warning', 'info', 'light', 'dark'
   def push_to_flash_alerts(text = '', type: 'info')
     flash[:alerts] ||= []
     flash[:alerts] << { 'type' => type.to_s, 'text' => text.to_s }
+  end
+
+  # Used by Sorcery
+  def not_authenticated
+    redirect_to login_path, alert: 'Please login first'
   end
 end
