a73b559ba56ae85b400e40765af5d7bd3cb369dd
Fizvlad <fizvlad@mail.ru>
2020-06-26 12:05:20 +0300

    Add logging in and out

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,9 @@ Rails.application.routes.draw do
   root to: 'pages#index'
 
   resources :users, except: %i[new index]
-  get 'signup', to: 'users#new'
+  get 'signup', to: 'users#new', as: 'signup'
+
+  resources :user_sessions, only: %i[create]
+  get 'login', to: 'user_sessions#new', as: 'login'
+  post 'logout', to: 'user_sessions#destroy', as: 'logout'
 end
