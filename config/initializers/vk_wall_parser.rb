9eeb7e0386422e7ecf385ad9aa40cd45ceda1fc9
Fizvlad <fizvlad@mail.ru>
2020-06-30 11:45:49 +0300

    Increase interval of updates

--- config/initializers/vk_wall_parser.rb
+++ config/initializers/vk_wall_parser.rb
@@ -2,7 +2,7 @@ require 'vk_wall_parser'
 
 ::VkWallParserRepeater = VkWallParser::Repeater.new(
   Rails.application.credentials[:vk][:api_key],
-  interval: 15.minutes,
+  interval: 2.hours,
   extended_parsing_once_in: 4
 )
 
