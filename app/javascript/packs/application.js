dfad64df2678d62b77a1ea59290c5b1b4cf174ce
Fizvlad <fizvlad@mail.ru>
2020-06-30 17:21:22 +0300

    Move images outside pack folder

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,7 +11,7 @@ require('channels')
 import './bootstrap/index.js'
 import '@fortawesome/fontawesome-free/js/all'
 
-const images = require.context('./images', true)
+const images = require.context('../images', true)
 const imagePath = (name) => images(name, true)
 
 document.addEventListener('turbolinks:load', () => {
