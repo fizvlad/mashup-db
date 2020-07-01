7a7b017c4a10567b9f4465007ffe3ade0b6592cd
Fizvlad <fizvlad@mail.ru>
2020-07-01 15:57:05 +0300

    Method for searching by name and retrieving list of mashups

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -2,4 +2,12 @@ class Artist < ApplicationRecord
   validates :name, presence: true
 
   has_many :audios, dependent: :destroy
+
+  def mashups
+    Mashup.joins(:audio).where('audios.artist_id = ?', [id])
+  end
+
+  def self.search(query)
+    self.where('name LIKE ?', "%#{sanitize_sql_like(query)}%")
+  end
 end
