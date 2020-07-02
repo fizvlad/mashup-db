7ff5402f48b66df15b18839ecfa91c00f3e0b94a
Fizvlad <fizvlad@mail.ru>
2020-07-02 18:17:37 +0300

    Add utility methods

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -7,8 +7,26 @@ class Artist < ApplicationRecord
     Mashup.joins(:audio).where('audios.artist_id = ?', id)
   end
 
+  def mashuper?
+    mashups.any?
+  end
+
   def posts
     Post.joins(mashups: :audio).where('audios.artist_id = ?', id)
+  end
+
+  def related_user_ids
+    re = posts.map(&:from_user)
+    re.compact!
+    re.uniq!
+    re
+  end
+
+  def related_club_ids
+    re = posts.map(&:from_club)
+    re.compact!
+    re.uniq!
+    re
   end
 
   def self.search(query)
